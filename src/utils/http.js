dd7054975d61333caa86872e2161caaeaec795a6
Ashutosh Gupta <ashutoshgupta312@gmail.com>
2021-12-26 01:49:28 +0530

    Added Axios, baseURL and interceptor for Axios

---
+++ src/utils/http.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const http = axios.create({
+  baseURL: "https://jobs-api.squareboat.info/api/v1/",
+});
+
+http.interceptors.request.use((config) => {
+  const token = JSON.parse(localStorage.getItem("user"))?.token || "";
+  if (!token) return config;
+
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default http;
