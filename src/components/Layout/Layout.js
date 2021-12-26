300ddcef05fa991e2c0dd056d038baa58ec920ee
Ashutosh Gupta <ashutoshgupta312@gmail.com>
2021-12-26 23:04:16 +0530

    Minor Changes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,17 @@
 import { Fragment } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../Header";
 
 const Layout = ({ children }) => {
+  const location = useLocation();
   return (
     <Fragment>
       <Header />
-      <div className="gradiant-bg absolute top-[-2px] z-[-1] w-full h-[464px]"></div>
+      <div
+        className={`gradiant-bg absolute top-[-2px] z-[-1] w-full ${
+          location.pathname === "/posts" ? "h-[210px]" : "h-[464px]"
+        }`}
+      ></div>
       {children}
     </Fragment>
   );
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -42,7 +42,7 @@ const Pagination = ({
             className="relative inline-flex items-center px-2 py-2 
                       rounded-md border border-gray-300 bg-white 
                       text-sm font-medium text-gray-650 hover:bg-gray-50
-                      disabled:border-primary/50 disabled:hover:bg-inherit"
+                      disabled:border-primary/50 disabled:bg-[#edf6ff] disabled:hover:bg-[#edf6ff]"
           >
             <span className="sr-only">Previous</span>
             <ChevronLeftIcon
@@ -79,7 +79,7 @@ const Pagination = ({
             className="relative inline-flex items-center px-2 py-2 
                       rounded-md border border-gray-300 bg-white 
                       text-sm font-medium text-gray-500 hover:bg-gray-50
-                      disabled:border-primary/50 disabled:hover:bg-inherit"
+                      disabled:border-primary/50 disabled:bg-[#edf6ff] disabled:hover:bg-[#edf6ff]"
           >
             <span className="sr-only">Next</span>
             <ChevronRightIcon
