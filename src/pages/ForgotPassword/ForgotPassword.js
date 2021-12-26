8052f5284f7ff6702dff987f4db68da3821c7e4f
Ashutosh Gupta <ashutoshgupta312@gmail.com>
2021-12-26 08:55:50 +0530

    Added Forgot Password API.

--- src/pages/ForgotPassword/ForgotPassword.js
+++ src/pages/ForgotPassword/ForgotPassword.js
@@ -1,8 +1,34 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 import { FormWrapper } from "../../components";
+import { authActions } from "../../redux/actions";
+
+const Schema = Yup.object().shape({
+  email: Yup.string().email("Invalid Email").required(),
+});
 
 const ForgotPassword = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: Schema,
+    onSubmit: async ({ email }) => {
+      try {
+        await dispatch(authActions.resetPassword({ email }));
+        navigate("/reset-password");
+      } finally {
+      }
+    },
+  });
+
   return (
-    <FormWrapper>
+    <FormWrapper handleSubmit={formik.handleSubmit}>
       <h2 className="text-primary font-medium text-2xl mb-5">
         Forgot your password?
       </h2>
@@ -11,22 +37,31 @@ const ForgotPassword = () => {
         instructions to reset your password.
       </p>
       <div className="mb-10">
-        <label class="block text-gray-700 text-sm mb-2" for="email">
+        <label className="block text-gray-700 text-sm mb-2" htmlFor="email">
           Email address
         </label>
         <input
-          class="appearance-none bg-gray-750/20 border border-gray-850 rounded 
-                w-full p-4 text-gray-700 leading-tight 
-                focus:outline-blue focus:shadow-outline"
+          className={`appearance-none bg-gray-750/20 border border-gray-850 rounded 
+                w-full p-4  leading-tight focus:shadow-outline
+                ${
+                  formik.touched.email && Boolean(formik.errors.email)
+                    ? " focus:outline-red-500 border-red-500"
+                    : " border-gray-850 focus:outline-blue"
+                }
+                `}
           id="email"
           type="email"
+          name="email"
           placeholder="Email address"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
       </div>
       <div className="flex justify-center">
         <button
-          class="bg-blue text-white py-3 px-14 rounded focus:outline-none focus:shadow-outline"
-          type="button"
+          className="bg-blue text-white py-3 px-14 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
         >
           Submit
         </button>
